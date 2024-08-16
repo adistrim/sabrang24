a7abef8f9ba71ae37eab9e004432c3582ba61f10
Aditya Raj <adistrim.dev@gmail.com>
2024-08-16 08:16:24 +0530

    background glass effect in navBar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,22 +1,31 @@
 'use client';
-
 import { motion } from 'framer-motion';
 import Link from 'next/link';
 
 const NavBar = () => {
-  const navItems = ['Home', 'About', 'Events', 'Schedule', 'Contact'];
+  const navItems = ['Home', 'About', 'Events', 'Schedule', 'Contact', 'Team'];
 
   return (
-    <nav className="bg-transparent p-6 z-10">
-      <ul className="flex justify-center space-x-8">
-        {navItems.map((item) => (
-          <motion.li key={item} whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}>
-            <Link href={`/${item.toLowerCase()}`} className="text-white z-50 text-xl font-semibold hover:text-yellow-300 transition-colors duration-300">
-              {item}
-            </Link>
-          </motion.li>
-        ))}
-      </ul>
+    <nav className="flex justify-center p-6 z-10">
+      <motion.div
+        className="bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-full px-8 py-4 shadow-lg"
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <ul className="flex space-x-8">
+          {navItems.map((item) => (
+            <motion.li key={item} whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}>
+              <Link
+                href={`/${item.toLowerCase()}`}
+                className="text-white z-50 text-lg font-semibold hover:text-yellow-300 transition-colors duration-300"
+              >
+                {item}
+              </Link>
+            </motion.li>
+          ))}
+        </ul>
+      </motion.div>
     </nav>
   );
 };
